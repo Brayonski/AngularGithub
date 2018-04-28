b437dc65ee83355c496af5719b8a2962b094b4e2
root <root@localhost.localdomain>
2018-04-28 14:56:13 +0300

    initial commit

---
+++ src/app/profile.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class ProfileService {
+
+  private username:string;
+  // private clientid = '';
+  // private clientsecret = '';
+
+  constructor(private http:Http) { 
+    console.log("Service in now ready!");
+    this.username = 'Brayonski';
+  }
+
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username)
+  .map(res => res.json());
+  }
+
+}
