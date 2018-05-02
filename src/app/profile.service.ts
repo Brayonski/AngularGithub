c21fda5577e54e214a30f75fb28a744d8c271d32
root <root@localhost.localdomain>
2018-05-02 10:38:25 +0300

    added clientsecret and clientid

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -7,8 +7,8 @@ import 'rxjs/add/operator/map';
 export class ProfileService {
 
   private username:string;
-  // private clientid = '';
-  // private clientsecret = '';
+  private clientid = '035d36eff237eb5bf8a9';
+  private clientsecret = '2321027e12d9aaf7740d7917b7e066b68c76ef34';
 
   constructor(private http:Http) { 
     console.log("Service in now ready!");
@@ -16,12 +16,12 @@ export class ProfileService {
   }
 
   getProfileInfo(){
-    return this.http.get("https://api.github.com/users/" + this.username)
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
   .map(res => res.json());
   }
 
   getProfileRepos(){
-    return this.http.get("https://api.github.com/users/" + this.username + "/repos")
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos" + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
   .map(res => res.json());
   }
 
